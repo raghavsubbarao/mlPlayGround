d2fa0b1a99d1d95af4d507a1b6ee9e5c17638d95
raghavsubbarao <37404838+raghavsubbarao@users.noreply.github.com>
2024-11-27 21:10:16 +0000

    Fixed bugs and naming ocnventions

--- src/mlPlayGround/generative/normFlow.py
+++ src/mlPlayGround/generative/normFlow.py
@@ -1,71 +1,10 @@
 import abc
-from typing import Optional, Iterable, Union, overload
+from typing import Optional, Iterable, Union, overload, Tuple
 
 import numpy as np
 import torch
 
-from core.ml.model import baseTorchModel, baseLossTracker
-
-
-#################################
-#               VAE             #
-#################################
-class variationalAutoencoder(baseTorchModel):
-    def __init__(self, encoder, decoder, beta=1.0, **kwargs):
-        # initialization
-        super(variationalAutoencoder, self).__init__(**kwargs)
-        self.encoder = encoder
-        self.decoder = decoder
-
-        # beta = 1.0 for Kingma, Welling but can be adjusted
-        # for beta-VAEs. beta > 1 allows disentangling generative factors
-        self.beta = beta
-
-        self.total_loss_tracker = baseLossTracker(name="total_loss")
-        self.recon_loss_tracker = baseLossTracker(name="recon_loss")
-        self.kl_loss_tracker = baseLossTracker(name="kl_loss")
-
-    @property
-    def metrics(self):
-        return [self.total_loss_tracker,
-                self.recon_loss_tracker,
-                self.kl_loss_tracker]
-
-    def encode(self, x):
-        mean, logvar = torch.split(self.encoder(x), split_size_or_sections=2, dim=1)
-        return mean, logvar
-
-    def decode(self, z):
-        return self.decoder(z)
-
-    def forward(self, inputs):
-        z_mean, z_logvar = self.encode(inputs)
-        z = torch.normal(torch.zeros(z_mean.shape), torch.ones(z_mean.shape)).to(self.device)
-        recon = self.decoder(z_mean + torch.exp(z_logvar / 2.) * z)
-        return recon, z_mean, z_logvar
-
-    def train_step(self, data, optimizer):
-        X = data.to(self.device)
-
-        recon, z_mean, z_log_var = self.forward(X)
-
-        recon_loss = torch.mean(torch.sum(torch.nn.functional.binary_cross_entropy(recon, X, reduction='none'), dim=[1, 2, 3]))
-        kl_loss = torch.mean(torch.sum((torch.exp(z_log_var) + torch.square(z_mean) - z_log_var) / 2., dim=1))
-        total_loss = recon_loss + self.beta * kl_loss
-
-        # Backpropagation
-        total_loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        self.total_loss_tracker.update_state(total_loss)
-        self.recon_loss_tracker.update_state(recon_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
-
-        return {"total_loss": self.total_loss_tracker.result(),
-                "recon_loss": self.recon_loss_tracker.result(),
-                "kl_loss": self.kl_loss_tracker.result()}
-
+from mlPlayGround.model import baseTorchModel, baseLossTracker
 
 #################################
 #            Real NVP           #
@@ -78,13 +17,19 @@ class normFlowModule(torch.nn.Module):
     there is no equivalent.
     This class aims to do something similar. To keep all the
     functionality offered by nn.Module we derive from it, but require
-    that all the necessary functions be defined in subclasses to make
-    sure that an concrete class can ultimately handle the required
-    computations!
+    that an abstract function to calculate the inverse log of the
+    determinant of the Jacobian (!!!)the necessary functions be defined
+    in subclasses to make sure that an concrete class can ultimately
+    handle the required computations!
     """
 
     @abc.abstractmethod
     def inverseLogDetJacobian(self, y: torch.Tensor, **kwargs) -> torch.Tensor:
+        """
+        :param y: B x ... tensor where B=batch_size
+        :param kwargs:
+        :return: tensor of size (B,) of inverse log determinant of the Jacobians
+        """
         pass
 
 
@@ -102,28 +47,27 @@ class normFlowSequential(torch.nn.Sequential, normFlowModule):
         super(normFlowSequential, self).__init__(*args)
 
     def inverseLogDetJacobian(self, y: torch.Tensor, **kwargs) -> torch.Tensor:
-        ll = 0
+        ll = torch.zeros(y.shape[0])
         for module in self:
             ll = ll + module.forwardLogDetJacobian(y)
             y = module(y)
         return ll
 
-    def forward(self, y: torch.Tensor):
+    def forward(self, y: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         ll = []
         for module in self:
             y, fldj = module(y)
             ll.append(fldj)
 
-        ll = torch.sum(torch.stack(ll, -1), 1)
-        return y, ll
+        return y, torch.sum(torch.stack(ll, -1), 1)
 
 
 class nvpBatchNorm2d(torch.nn.modules.BatchNorm2d, normFlowModule):
     """
-    Implementation of batch norm for real NVP. We use the batch norm in two
-    places (i) the scale and shift sub networks - this is trivial and torch's
-    implementation would be fine. (ii) in the coupling layers - here we need
-    to account for the batch norm in the loss function.
+    Implementation of batch norm for real NVP. We use the batch norm in two places
+    (i) the scale and shift sub networks - this is trivial and torch's
+    implementation works. (ii) in the coupling layers - here we need to account
+    for the batch norm in the loss function which is what this class is for!
     """
 
     def __init__(self, num_features: int, eps: float = 1e-5,
@@ -132,22 +76,29 @@ class nvpBatchNorm2d(torch.nn.modules.BatchNorm2d, normFlowModule):
         super(nvpBatchNorm2d, self).__init__(num_features, eps, momentum, affine,
                                              track_running_stats, device, dtype)
 
-    def inverseLogDetJacobian(self, y: torch.Tensor) -> torch.Tensor:
+    def inverseLogDetJacobian(self, y: torch.Tensor, **kwargs) -> torch.Tensor:
         lp = torch.log(self.weight) if self.affine else 0
         fldj = lp - 0.5 * torch.log(self.running_var + self.eps)
         return torch.sum(torch.flatten(fldj.view([1, self.num_features, 1, 1]).expand_as(y), 1), 1)
 
-    def forward(self, input: torch.Tensor):
-        return super(nvpBatchNorm2d, self).forward(input), self.inverseLogDetJacobian(input)
+    def forward(self, y: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        return super(nvpBatchNorm2d, self).forward(y), self.inverseLogDetJacobian(y)
 
 
 class realNVPCouplingLayer(normFlowModule):
+    """
+    Single NVP coupling layer. Implements the checkerboard and channel
+    masks from [Dinh 2016] which can be inverted using the 'flip' input
+    to the constructor.
+    TODO: Add regularization!
+    """
     def __init__(self,
                  scaleModule: torch.nn.Module,
                  biasModule: torch.nn.Module,
                  dims: Iterable[int],
                  mask: str,
                  flip: Union[int, bool],
+                 weightDecay=5e-5,
                  **kwargs):
         super(realNVPCouplingLayer, self).__init__(**kwargs)
 
@@ -157,6 +108,8 @@ class realNVPCouplingLayer(normFlowModule):
         self.sScale = torch.nn.Parameter(torch.zeros(dims), requires_grad=True)
         self.tBias = torch.nn.Parameter(torch.zeros(dims), requires_grad=True)
         self.tScale = torch.nn.Parameter(torch.zeros(dims), requires_grad=True)
+
+        self.weightDecay = weightDecay
 
         if mask == 'check':
             mask = self.checkerBoardMask(dims)
@@ -169,27 +122,28 @@ class realNVPCouplingLayer(normFlowModule):
         self.register_buffer(name='mask', tensor=mask)
 
     @staticmethod
-    def checkerBoardMask(dims):
+    def checkerBoardMask(dims: Iterable[int]) -> torch.Tensor:
         return torch.Tensor(1 - np.indices(dims).sum(axis=0) % 2)
 
     @staticmethod
-    def channelMask(dims):
-        assert len(dims) == 3
-        assert dims[0] % 2 == 0
+    def channelMask(dims: Iterable[int]) -> torch.Tensor:
+        assert(len(dims) == 3)
+        assert(dims[0] % 2 == 0)
         mask = torch.cat([torch.zeros((dims[0] // 2, dims[1], dims[2])),
                           torch.ones((dims[0] // 2, dims[1], dims[2]))], dim=0)
-        assert mask.shape == dims
+        assert(mask.shape == dims)
         return mask
 
-    def inverseLogDetJacobian(self, y: torch.Tensor) -> torch.Tensor:
+    def inverseLogDetJacobian(self, y: torch.Tensor, **kwargs) -> torch.Tensor:
         s = self.sScale * torch.tanh(self.s(self.mask * y))
         return torch.sum(torch.flatten((1 - self.mask) * s, 1), 1)
 
-    def forward(self, y: torch.Tensor) -> torch.Tensor:
+    def forward(self, y: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         t = self.tScale * self.t(self.mask * y) + self.tBias
         s = self.sScale * torch.tanh(self.s(self.mask * y))
         out = self.mask * y + (1 - self.mask) * (y * torch.exp(s) + t)
         ll = torch.sum(torch.flatten((1 - self.mask) * s, 1), 1)
+        ll = ll - self.weightDecay * torch.sum(self.sScale) / y.shape[0]
         return out, ll
 
 
@@ -202,7 +156,7 @@ class realNonVolumePreserving(baseTorchModel):
         self.channelList = torch.nn.ModuleList()
         self.register_buffer('pi', torch.tensor(np.pi))
 
-        for i in range(nScales):
+        for _ in range(nScales):
             # each scale of the hierarchy consists of:
             # 1. three coupling layers with alternating checkerboard masks
             # 2. A squeeze operations s x s x c --> s/2 x s/2 x 4c
@@ -243,13 +197,15 @@ class realNonVolumePreserving(baseTorchModel):
                                                                                                   dims, 'check', i % 2 == 0),
                                                                              nvpBatchNorm2d(dims[0], affine=False)]])
 
-        self.total_loss_tracker = baseLossTracker(name="total_loss")
-        self.log_loss_tracker = baseLossTracker(name="log_loss")
-        self.ll_loss_tracker = baseLossTracker(name="ll_loss")
+        self.totalLossTracker = baseLossTracker(name="totalLoss")
+        self.logLossTracker = baseLossTracker(name="logLoss")
+        self.priorLossTracker = baseLossTracker(name="priorLoss")
 
     @property
     def metrics(self):
-        return [self.loss_tracker]
+        return [self.totalLossTracker,
+                self.logLossTracker,
+                self.priorLossTracker]
 
     def forward(self, y):
         yr = []
@@ -273,27 +229,36 @@ class realNonVolumePreserving(baseTorchModel):
 
         return torch.flatten(torch.cat(yr, 1), 1), torch.sum(torch.stack(ll, -1), 1)
 
-    def train_step(self, data, optimizer):
+    def trainStep(self, data, optimizer):
         X = data.to(self.device)
-
         y, ll = self.forward(X)
 
-        log_loss = torch.sum((torch.log(2 * self.pi) + y * y) / 2.)
-        ll_loss = -torch.sum(ll)
-        total_loss = log_loss + ll_loss
+        priorLoss = torch.mean((torch.log(2 * self.pi) + y * y) / 2.)
+        logLoss = -torch.mean(ll)
+        totalLoss = priorLoss + logLoss
 
         # Backpropagation
-        total_loss.backward()
-        optimizer.step()
         optimizer.zero_grad()
+        totalLoss.backward()
+        optimizer.step()
 
-        self.total_loss_tracker.update_state(total_loss)
-        self.log_loss_tracker.update_state(log_loss)
-        self.ll_loss_tracker.update_state(ll_loss)
+        self.totalLossTracker.updateState(totalLoss)
+        self.logLossTracker.updateState(logLoss)
+        self.priorLossTracker.updateState(priorLoss)
 
-        return {"total_loss": self.total_loss_tracker.result(),
-                "log_loss": self.log_loss_tracker.result(),
-                "ll_loss": self.ll_loss_tracker.result()}
+        return {"totalLoss": self.totalLossTracker.result(),
+                "logLoss": self.logLossTracker.result(),
+                "priorLoss": self.priorLossTracker.result()}
+
+    def validStep(self, data):
+        X = data.to(self.device)
+        y, ll = self.forward(X)
+
+        priorLoss = torch.mean((torch.log(2 * self.pi) + y * y) / 2.)
+        logLoss = -torch.mean(ll)
+        totalLoss = priorLoss + logLoss
+
+        return totalLoss
 
 
 if __name__ == "__main__":
